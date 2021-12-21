bb0ab38da2e055df2ad17c8c891bdd163d6c00a8
Seongheon Sim <river.water.sea@gmail.com>
2021-12-22 01:43:10 +0900

    #2 틱택토 게임 - 1
    complete > Player 추가, 승자 확인, Square Class -> Function 변경
    incomplete > 말 이동 기록 남기기 기능, Class 와 Function 컴포넌트의 차이

--- react-getting-started/src/square/square.js
+++ react-getting-started/src/square/square.js
@@ -1,24 +1,15 @@
 import '../square/square.css';
 import React from 'react';
 
-class Square extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            value: null,
-        };
-    }
-
-    render() {
-        return (
-            <button 
-                className='square' 
-                onClick={() => this.props.onClick()}
-            >
-                {this.state.value}
-            </button>
-        );
-    }
+function Square(props) {
+    return (
+        <button 
+            className='square' 
+            onClick={props.onClick}
+        >
+            {props.value}
+        </button>
+    );
 }
 
 export default Square;
